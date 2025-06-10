93df35452a2df052d82aa70cba950affad7ce9e1
Alif Abdullah <alifabdullah7040@gmail.com>
2025-06-10 10:38:46 -0400

    Setup navbar with search bar on top

--- fe/src/components/Header/index.tsx
+++ fe/src/components/Header/index.tsx
@@ -6,8 +6,25 @@ type HeaderProps = {
 export default function Header() {
   return (
     <>
-      <div className="w-screen bg-slate-900">
-        Header
+
+      <div id="nav-slate-top">
+        <div className="flex w-screen bg-slate-900 h-fit">
+          <div>
+            Left
+          </div>
+
+          <div className="flex-1">
+            <input type="text" className="bg-white m-4 rounded-md placeholder:text-gray-700 placeholder:pl-2" placeholder="Search Yippieazon"></input>
+          </div>
+
+          <div>
+            Right
+          </div>
+        </div>
+      </div>
+
+      <div id="nav-dark-below-slate-top">
+
       </div>
     </>
   )
